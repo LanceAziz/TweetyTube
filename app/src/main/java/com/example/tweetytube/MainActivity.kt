abcaa73b353ebf92e2bb3171de00a2de551d32bd
lance aziz <lanceaziz2002@gmail.com>
2024-12-03 00:53:50 +0200

    Icon and Splash Screen Done

--- app/src/main/java/com/example/tweetytube/MainActivity.kt
+++ app/src/main/java/com/example/tweetytube/MainActivity.kt
@@ -8,40 +8,27 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.tweetytube.ui.theme.TweetyTubeTheme
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.tweetytube.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
-            TweetyTubeTheme {
+            AppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    Text(
+                        text = "Feed",
+                        modifier = Modifier.padding(innerPadding),
+                        fontSize = 64.sp
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TweetyTubeTheme {
-        Greeting("Android")
     }
 }
