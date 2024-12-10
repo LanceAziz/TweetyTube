8b630a66cedb35d8ed35b4be2064712657a484e0
lance aziz <lanceaziz2002@gmail.com>
2024-12-10 18:28:12 +0200

    stable

--- app/build/tmp/kapt3/stubs/debug/com/example/tweetytube/features/movieList/presentation/components/movieCard/MovieCardKt.java
+++ app/build/tmp/kapt3/stubs/debug/com/example/tweetytube/features/movieList/presentation/components/movieCard/MovieCardKt.java
@@ -10,11 +10,11 @@ import com.example.tweetytube.R;
 import com.example.tweetytube.movie_list.data.repo.local.Movie;
 import com.example.tweetytube.ui.theme.*;
 
-@kotlin.Metadata(mv = {1, 9, 0}, k = 2, xi = 48, d1 = {"\u0000\u0014\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u000b\n\u0000\u001a\u0018\u0010\u0000\u001a\u00020\u00012\u0006\u0010\u0002\u001a\u00020\u00032\u0006\u0010\u0004\u001a\u00020\u0005H\u0007\u00a8\u0006\u0006"}, d2 = {"MovieCard", "", "movie", "Lcom/example/tweetytube/movie_list/data/repo/local/Movie;", "loading", "", "app_debug"})
+@kotlin.Metadata(mv = {1, 9, 0}, k = 2, xi = 48, d1 = {"\u0000\u000e\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\u001a\u0010\u0010\u0000\u001a\u00020\u00012\u0006\u0010\u0002\u001a\u00020\u0003H\u0007\u00a8\u0006\u0004"}, d2 = {"MovieCard", "", "movie", "Lcom/example/tweetytube/movie_list/data/repo/local/Movie;", "app_debug"})
 public final class MovieCardKt {
     
     @androidx.compose.runtime.Composable()
     public static final void MovieCard(@org.jetbrains.annotations.NotNull()
-    com.example.tweetytube.movie_list.data.repo.local.Movie movie, boolean loading) {
+    com.example.tweetytube.movie_list.data.repo.local.Movie movie) {
     }
 }
